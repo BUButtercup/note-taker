54d3aff8f23d384a055b3957774b558526323965
Buttercup <andyandnile@gmail.com>
2022-02-03 20:14:30 -0800

    adding routes

--- Develop/Routes/server.js
+++ Develop/Routes/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const _dirname = path.resolve();
 const fs = require('fs');
+const {v4: uuidv4} = require('uuid');
 
 const notes = require('../db/db.json')
 
@@ -21,7 +22,40 @@ app.get('/notes', (req, res) => res.sendFile(path.join(_dirname, '../public/note
 
 // app.post('/notes', )
 
-app.get('/api/notes', (req, res) => res.json(notes));
+app.get('/api/notes', (req, res) => {
+    console.log(`${req.method} request made for notes list`);
+    fs.readFile('../db/db.json', 'utf-8', (err, data) => {
+        if(err){
+            res.status(500).send(`Request couldn't be completed`)
+            throw(err)
+        } else{
+        res.json(JSON.parse(data));
+        console.log(`Status ${res.statusCode}, info sent`)
+        }
+    })
+});
+
+app.get('/api/notes/:id', (req, res) => {
+    console.log(`${req.method} request made for note ${req.params.id}`);
+    fs.readFile('../db/db.json', 'utf-8', (err, data) => {
+        if(err){
+            res.status(500).send(`Request couldn't be completed`)
+            throw(err)
+        } else{
+            const noteArr = JSON.parse(data);
+            console.log(noteArr);
+            const note = noteArr.find(item => {
+                if(item.id === req.params.id){
+                    res.json(item)
+                    return item
+                }
+            })
+            if(!note){
+                res.status(404).send(`No note with ID ${req.params.id} in list`)
+            }
+        }
+    })
+})
 
 app.post('/api/notes', (req, res) => {
     console.log(`${req.method} request recieved to add note.`);
@@ -29,36 +63,63 @@ app.post('/api/notes', (req, res) => {
         res.status(404).send('Input must include a title.');
         console.log(res.status);
     }
-
     console.log(req.body);
-
-    const {title, note} = req.body;
-
-
     const newNote = {
+        id: uuidv4(),
         title: req.body.title,
-        note: req.body.text
+        note: req.body.note
     }
-
     console.log(newNote)
-
+    console.log(`Did it! newNote: ${newNote.title} / ${res.statusCode}`);
     fs.readFile('../db/db.json', 'utf-8', (err, data) => {
         if(err){throw(err)} else{
             const noteBund = JSON.parse(data);
             noteBund.push(newNote)
+            console.log(noteBund);
             fs.writeFile('../db/db.json', JSON.stringify(noteBund), err => {
                 if(err){
                     throw err;
                 }
                 console.log('written');
+                res.json(newNote)
             })
         };
-      
     })
-
-    console.log(`Did it! newNote: ${newNote} / ${res.statusCode}`);
- 
 })
+
+app.delete('/api/notes/:id', (req, res)=>{
+    console.log(`${req.method} request recieved to delete note.`);
+    fs.readFile('../db/db.json', 'utf-8', (err, data) => {
+        try{    
+            console.log(data);
+            const noteArr = JSON.parse(data);
+            console.log(noteArr);
+            noteArr.filter(note => {
+                if(note.id === req.params.id){
+                    console.log(note)
+                    const i = noteArr.indexOf(note)
+                    noteArr.splice(i, 1);
+                    console.log(noteArr)
+                    fs.writeFile('../db/db.json', JSON.stringify(noteArr), err => {
+                        if(err){
+                            throw err;
+                        }
+                        console.log('written');
+                    })
+                    return res.send(`${note.title} has been deleted!`)
+                }
+            })
+        } catch (error) {
+            res.status(500).send(`Request couldn't be completed. Error: ${error}`)
+            throw(error)
+        } 
+    })
+})
+
+// app.put('/api/notes/:id', (req, res) => {
+//     console.log(`${req.method} request recieved to edit note.`);
+//     fs
+// })
 
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}.`));
